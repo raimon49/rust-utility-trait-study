252e35f1aaa43e88776c840294b5667d66807770
raimon <raimon49@hotmail.com>
2021-01-31 16:29:45 +0900

    Implement 'Drop' trait for my 'Application' type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+struct Application {
+    name: String,
+    nicknames: Vec<String>
+}
+
+impl Drop for Application {
+    fn drop(&mut self) {
+        print!("Dropping {}", self.name);
+        if !self.nicknames.is_empty() {
+            print!(" (AKA {})", self.nicknames.join(", "));
+        }
+
+        println!("");
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
