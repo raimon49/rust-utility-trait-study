fe9e3bee3eb6ecaec4ab50df7d406bd98104d658
raimon <raimon49@hotmail.com>
2021-02-06 16:43:15 +0900

    Add a comment about std::ops::Drep trait

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,13 @@ struct Application {
 }
 
 impl Drop for Application {
+    // 値がヒープから解放されるタイミングでDropトレイトのdrop()が呼び出される
+    // C++のデストラクタやJavaのファイナライザに似ている
+    // dropメソッドは暗黙的呼び出ししか許可されず、自分で呼び出そうとするとエラーになる
     fn drop(&mut self) {
         print!("Dropping {}", self.name);
         if !self.nicknames.is_empty() {
+            // drop()で呼ばれたタイミングでは、まだ値が入っているため利用できる
             print!(" (AKA {})", self.nicknames.join(", "));
         }
 
